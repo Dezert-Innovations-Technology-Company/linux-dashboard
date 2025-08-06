d646f3853008d75cf9d88db8da16b198d45ed63f
Afnan <alomiria@dezert.sa>
2025-08-06 12:48:24 +0300

    Update database.py
    #  DATABASE_URL from .env
    SQLALCHEMY_DATABASE_URL = DATABASE_URL

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:123456789@localhost/QuraanAppUsers"
+# DATABASE_URL from .env
+SQLALCHEMY_DATABASE_URL = DATABASE_URL
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
