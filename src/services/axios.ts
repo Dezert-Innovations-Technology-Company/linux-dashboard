226e461e3eb563a60136c2d4984b9273f4a1f904
Afnan <alomiria@dezert.sa>
2025-08-07 15:56:41 +0300

    Update axios.ts

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  // baseURL: '/api',
-  baseURL: 'http://127.0.0.1:8000',
+  baseURL: '/api',
+  // baseURL: 'http://127.0.0.1:8000',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
